1c01671630c45c46d3e892cfd0f695937e329314
1804449@kiit.ac.in <1804449@kiit.ac.in>
2020-10-21 20:15:07 +0530

    Random Pasword Generator

--- src/components/MainComp.js
+++ src/components/MainComp.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Interprot from "./Interprot";
 import Och from "./Och";
+import Sf from "./sf";
 import Tmz from "./tmz";
+import Header from "./Header";
+import Display from "./Display";
 
 export default function Maincomp() {
   return (
     <>
-      <Och />
+      <Header />
+      <Display />
     </>
   );
 }
---
+++ src/components/sf.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Sf extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentWillMount() {}
+
+  render() {
+    return <></>;
+  }
+}
